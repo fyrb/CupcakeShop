ede7ea00d02f97d902a2191ea1e5a8bc3a2c58de
KP <fyrb_38@hotmail.com>
2017-07-04 15:05:05 +0200

    Lab05Nr01

---
+++ CupcakeShop/cupcakeshop/src/main/java/helloworld/HelloWorldRest.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+
+
+@Path("/hi")
+public class HelloWorldRest {
+	@GET
+	public String getMsg(){
+		return "Hello World from Jersey";
+	}
+
+}
